3e9163adcb64cdb810b084baef7060774d9b0bac
alexey <aoklyunin@gmail.com>
2018-01-06 17:59:43 +0300

    initial

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from urllib.request import urlretrieve
 
+cnt = 0
 for i in range(0,255):
     for j in range(12,15):
         try:
             name = ("D83DD"+format(j, 'x')+format(i, 'x')).upper()
             print(name)
             href = "http://vk.com/images/emoji/"+name+".png"
-            urlretrieve(href,"smiles/"+name+".png")
+            urlretrieve(href,"smiles/smile"+str(cnt)+".png")
+            cnt+=1
             print(href)
         except:
             print('not found')
